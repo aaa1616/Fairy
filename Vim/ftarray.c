a74b3391ed29ca99f58e387c657903d95d8b3b5a
Fairy <Fairy@Tail.com>
2015-01-06 23:11:50 +0800

    array init and free

--- Vim/ftarray.c
+++ Vim/ftarray.c
@@ -2,14 +2,29 @@
 
 int ftInit2DArray(void ***parray, int row, int col, int size)
 {
-	void * tdarray = NULL;
-	if ((tdarray = ftMalloc(row * col * size)) == NULL) {
+	void ** prow = NULL;
+	void * parray = NULL;
+	if ((prow = ftMalloc(row * size)) == NULL) {
 		return OUTOFMEM_STATUS;
 	}
+
+	if ((parray = ftMalloc(row * col * size)) == NULL) {
+		ftFree(prow);
+		return OUTOFMEM_STATUS;
+	}
+	
+	memset(parray, 0, row * col * size);
+	for (int i = 0; i < row; i++) {
+		prow[i] = (parray + i * col * size);
+	}
+
+	*parray = prow;
 
 	return SUCCESS_STATUS;
 }
 int ftFree2DArray(void ***parray)
 {
+	ftFree((*parray)[0]);
+	ftFree(*parray);
 	return SUCCESS_STATUS;
 }
--- Vim/ftmem.h
+++ Vim/ftmem.h
@@ -1,6 +1,8 @@
 #ifndef _FTMEM_H
 #define _FTMEM_H
 
+#include <memory.h>
+
 #define ftMalloc(isize) malloc(isize)
 #define ftFree(pname) free(pname)
 
