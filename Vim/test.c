097120d359174474311f83e4bebc406f3b26af49
Fairy <Fairy@Tail.com>
2015-04-06 22:27:25 +0800

    compiler sizeof

--- Vim/test.c
+++ Vim/test.c
@@ -1,92 +1,93 @@
-//#include "ftinclib.h"
-//#include <time.h>
-//#include <io.h>
-//#include "aidl.h"
-//#include "graph.h"
-//#include "largetnum.h"
-//#include "nqueen.h"
-//
-//#define FILE_ITEM 200
-//#define FUNC_ITEM 100
-//#define BUFF_SIZE 200
-//
-////char aidl_table[200][100][200];
-//int main(void)
-//{
-//	//char ***aidl_table = malloc(FILE_ITEM * sizeof (char *));
-//	//char **aidl_table_2d = malloc(FUNC_ITEM * FILE_ITEM * sizeof(char *));
-//	//char *aidl_table_3d = malloc(FUNC_ITEM * FILE_ITEM * BUFF_SIZE * sizeof(char *));
-//	//memset(aidl_table_3d, 0, FUNC_ITEM * FILE_ITEM * BUFF_SIZE);
-//	//for (int i = 0; i < FILE_ITEM; i++) {
-//	//	aidl_table[i] = aidl_table_2d + i * FUNC_ITEM;
-//	//}
-//	//for (int i = 0; i < FUNC_ITEM * FILE_ITEM; i++) {
-//	//	aidl_table_2d[i] = aidl_table_3d + i * BUFF_SIZE;
-//	//}
-//	//char * dir = "E:\\aidl\\*.*";
-//	//long handle;
-//	//struct _finddata_t fileinfo;
-//	//handle = _findfirst(dir, &fileinfo);
-//	//int total = 0;
-//	//int func = 0;
-//	//fnHash = NULL;
-//	//if (handle != -1) {
-//	//	while (!_findnext(handle, &fileinfo)) {
-//	//		char filePath[256] = "E:\\aidl\\";
-//	//		strcat(filePath, fileinfo.name);
-//	//		FILE *file = fopen(filePath, "r");
-//	//		if (file != NULL) {
-//	//			if ((func = parseAidl(file, fileinfo.name, total, aidl_table)) > 1) {
-//	//				total++;
-//	//				//for (int i = 1; i < func; i++) {
-//	//				//	printf("%s\n",aidl_table[total - 1][i]);
-//	//				//}
-//	//				//printf("%s\n", filePath);
-//	//			}
-//	//		}
-//	//	}
-//	//}
-//	//char des[100];
-//	//scanf("%s", des);
-//	//struct my_struct *s;
-//	//HASH_FIND_STR(fnHash, des, s);
-//	//FILE *file =fopen("E:\\aidl\\filelist", "r");
-//	//char filelist[500][100];
-//	//int i = sizeof(filelist);
-//	//memset(filelist, 0, 500 * 100 * sizeof(char));
-//	//if (file != NULL) {
-//	//	getAidlFilename(file, filelist);
-//	//}
-//
-//	////printf("%s\n", aidl_table[s->id][1]);
-//	//divide(INT_MAX, 1);
-//	//GRAPH *g = createGraph();
-//	//dfs(g);
-//	//bfs(g, 0);
-//	//delGraph(g);
-//	//FILE *file = fopen("input.txt", "r");
-//	//int n;
-//	//myscanf(file, "%d", &n);
-//	//nqueen(n);
-//	
-//	//int num[10];
-//	//for (int i = 0; i < n; i++) {
-//	//	myscanf(file, "%d", num + i);
-//	//}
-//	//fclose(file);
-//	//printf("%d\n", selectKth(num, 0, n, 8));
-//	//int *ret = countSort(num, n, 6);
-//	//for (int i = 0; i < n; i++) {
-//	//	printf("%d ", ret[i]);
-//	//}
-//	//printf("\n");
-//	//free(ret);
-//
-//	//BINTREE *bt = createBinTreeLR();
-//	//preOrder(bt->root);
-//	//printf("\n");
-//	//delBinTree(bt->root);
-//	//free(bt);
-//
-//	system("pause");
-//}
+#include "ftinclib.h"
+#include <time.h>
+#include <io.h>
+#include "aidl.h"
+#include "graph.h"
+#include "largetnum.h"
+#include "nqueen.h"
+
+#define FILE_ITEM 200
+#define FUNC_ITEM 100
+#define BUFF_SIZE 200
+
+//char aidl_table[200][100][200];
+int main(void)
+{
+	//char ***aidl_table = malloc(FILE_ITEM * sizeof (char *));
+	//char **aidl_table_2d = malloc(FUNC_ITEM * FILE_ITEM * sizeof(char *));
+	//char *aidl_table_3d = malloc(FUNC_ITEM * FILE_ITEM * BUFF_SIZE * sizeof(char *));
+	//memset(aidl_table_3d, 0, FUNC_ITEM * FILE_ITEM * BUFF_SIZE);
+	//for (int i = 0; i < FILE_ITEM; i++) {
+	//	aidl_table[i] = aidl_table_2d + i * FUNC_ITEM;
+	//}
+	//for (int i = 0; i < FUNC_ITEM * FILE_ITEM; i++) {
+	//	aidl_table_2d[i] = aidl_table_3d + i * BUFF_SIZE;
+	//}
+	//char * dir = "E:\\aidl\\*.*";
+	//long handle;
+	//struct _finddata_t fileinfo;
+	//handle = _findfirst(dir, &fileinfo);
+	//int total = 0;
+	//int func = 0;
+	//fnHash = NULL;
+	//if (handle != -1) {
+	//	while (!_findnext(handle, &fileinfo)) {
+	//		char filePath[256] = "E:\\aidl\\";
+	//		strcat(filePath, fileinfo.name);
+	//		FILE *file = fopen(filePath, "r");
+	//		if (file != NULL) {
+	//			if ((func = parseAidl(file, fileinfo.name, total, aidl_table)) > 1) {
+	//				total++;
+	//				//for (int i = 1; i < func; i++) {
+	//				//	printf("%s\n",aidl_table[total - 1][i]);
+	//				//}
+	//				//printf("%s\n", filePath);
+	//			}
+	//		}
+	//	}
+	//}
+	//char des[100];
+	//scanf("%s", des);
+	//struct my_struct *s;
+	//HASH_FIND_STR(fnHash, des, s);
+	//FILE *file =fopen("E:\\aidl\\filelist", "r");
+	//char filelist[500][100];
+	//int i = sizeof(filelist);
+	//memset(filelist, 0, 500 * 100 * sizeof(char));
+	//if (file != NULL) {
+	//	getAidlFilename(file, filelist);
+	//}
+
+	////printf("%s\n", aidl_table[s->id][1]);
+	//divide(INT_MAX, 1);
+	//GRAPH *g = createGraph();
+	//dfs(g);
+	//bfs(g, 0);
+	//delGraph(g);
+	//FILE *file = fopen("input.txt", "r");
+	//int n;
+	//myscanf(file, "%d", &n);
+	//nqueen(n);
+	
+	//int num[10];
+	//for (int i = 0; i < n; i++) {
+	//	myscanf(file, "%d", num + i);
+	//}
+	//fclose(file);
+	//printf("%d\n", selectKth(num, 0, n, 8));
+	//int *ret = countSort(num, n, 6);
+	//for (int i = 0; i < n; i++) {
+	//	printf("%d ", ret[i]);
+	//}
+	//printf("\n");
+	//free(ret);
+
+	//BINTREE *bt = createBinTreeLR();
+	//preOrder(bt->root);
+	//printf("\n");
+	//delBinTree(bt->root);
+	//free(bt);
+
+	printf("%d, %d", sizeof(__int64), sizeof(int *));
+	system("pause");
+}
